382f9de67a48560836830e707eabc4d83e4acb86
Bruno Dias <brunodias40@gmail.com>
2024-08-01 20:07:43 -0300

    feat: add put and delete route CRUD

--- src/routes.ts
+++ src/routes.ts
@@ -49,4 +49,44 @@ router.post("/users", async (request, response) => {
   }
 });
 
+router.put("/users/:id", async (request, response) => {
+  const { id } = request.params;
+  const { name, email, password } = request.body;
+
+  try {
+    const user = await User.findByPk(id);
+
+    if (user === null) {
+      return response.status(404).json({ message: "User not found" });
+    }
+
+    user.name = name;
+    user.email = email;
+    user.password = password;
+
+    await user.save();
+
+    return response.json(user);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.delete("/users/:id", async (request, response) => {
+  const { id } = request.params;
+
+  try {
+    const user = await User.findByPk(id);
+
+    if (user === null) {
+      return response.status(404).json({ message: "User not found" });
+    }
+
+    await user.destroy();
+    return response.status(202).json({ message: "User deleted" });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 export { router };
