01ff98b33c92624174a5eeb8822f5bbf4546617a
Bruno Dias <brunodias40@gmail.com>
2024-08-01 17:50:17 -0300

    feat: add User model

---
+++ src/models/user-model.ts
@@ -0,0 +1,40 @@
+import { sequelize } from "../database";
+import { DataTypes, Model } from "sequelize";
+
+interface UserInstance extends Model {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+}
+
+const User = sequelize.define<UserInstance>(
+  "users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    timestamps: true,
+  }
+);
+
+export { User };
