533cbd77f73f2918ddcf883af8c3ecf408a67e32
Bruno Dias <brunodias40@gmail.com>
2024-08-01 13:45:07 -0300

    feat: add routes file with home route

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
+});
+
+export { router };
