890e533317a01c1746bb4e6757c93afacc4ce288
Bruno Dias <brunodias40@gmail.com>
2024-08-01 17:48:52 -0300

    refactor: update server.ts to handle port dynamically using environment variable

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,10 @@ app.use(router);
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
-app.listen(port | 3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+try {
+  app.listen(port | 3000, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+} catch (error) {
+  console.error(error);
+}
