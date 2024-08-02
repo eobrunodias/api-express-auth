9986a29f3981a73473758900abf94d9e53a0e2ee
Bruno Dias <brunodias40@gmail.com>
2024-08-02 12:56:54 -0300

    feat: add controller and refactor routes

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,1 +1,87 @@
+import { User } from "../models/user-model";
+import { Request, Response } from "express";
 
+const usersController = {
+  index: async (req: Request, res: Response) => {
+    try {
+      const users = await User.findAll();
+
+      if (users.length === 0) {
+        return res.status(404).json({ message: "Users not found" });
+      }
+
+      return res.json(users);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  show: async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+
+      if (user === null) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      return res.json(user);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  save: async (req: Request, res: Response) => {
+    const { name, email, password } = req.body;
+
+    try {
+      const user = await User.create({ name, email, password });
+
+      return res.json(user);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  update: async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+
+    try {
+      const user = await User.findByPk(id);
+
+      if (user === null) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      user.name = name;
+      user.email = email;
+      user.password = password;
+
+      await user.save();
+
+      return res.json(user);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  delete: async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const user = await User.findByPk(id);
+
+      if (user === null) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      await user.destroy();
+      return res.status(202).json({ message: "User deleted" });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
+export { usersController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,92 +1,20 @@
 import { Router } from "express";
-import { User } from "./models/user-model";
+import { usersController } from "./controllers/user-controller";
 
 const router = Router();
 
+// Home
 router.get("/", (request, response) => {
   return response.json({ message: "Hello World" });
 });
 
-router.get("/users", async (request, response) => {
-  try {
-    const users = await User.findAll();
-    console.log(users);
+// Users
+router.get("/users", usersController.index);
+router.get("/users/:id", usersController.show);
+router.post("/users", usersController.save);
+router.put("/users/:id", usersController.update);
+router.delete("/users/:id", usersController.delete);
 
-    if (users.length === 0) {
-      return response.status(404).json({ message: "Users not found" });
-    }
-
-    return response.json(users);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-router.get("/users/:id", async (request, response) => {
-  try {
-    const { id } = request.params;
-    const user = await User.findByPk(id);
-
-    if (user === null) {
-      return response.status(404).json({ message: "User not found" });
-    }
-
-    return response.json(user);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-router.post("/users", async (request, response) => {
-  const { name, email, password } = request.body;
-
-  try {
-    const user = await User.create({ name, email, password });
-
-    return response.json(user);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-router.put("/users/:id", async (request, response) => {
-  const { id } = request.params;
-  const { name, email, password } = request.body;
-
-  try {
-    const user = await User.findByPk(id);
-
-    if (user === null) {
-      return response.status(404).json({ message: "User not found" });
-    }
-
-    user.name = name;
-    user.email = email;
-    user.password = password;
-
-    await user.save();
-
-    return response.json(user);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-router.delete("/users/:id", async (request, response) => {
-  const { id } = request.params;
-
-  try {
-    const user = await User.findByPk(id);
-
-    if (user === null) {
-      return response.status(404).json({ message: "User not found" });
-    }
-
-    await user.destroy();
-    return response.status(202).json({ message: "User deleted" });
-  } catch (error) {
-    console.error(error);
-  }
-});
+// ...
 
 export { router };
