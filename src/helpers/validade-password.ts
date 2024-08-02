22dc9d3a4c61b651405721fe7e0f0d5d19e27a51
Bruno Dias <brunodias40@gmail.com>
2024-08-02 13:44:03 -0300

    feat: add email and password validation helper function

---
+++ src/helpers/validade-password.ts
@@ -0,0 +1,31 @@
+import validator from "validator";
+
+export function validatePassword(password: string): string | boolean {
+  const isLengthValid = validator.isLength(password, { min: 8 });
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasDigit = /[0-9]/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  if (!isLengthValid) {
+    return "Password must have at least 8 characters";
+  }
+
+  if (!hasUpperCase) {
+    return "Password must have at least 1 uppercase character";
+  }
+
+  if (!hasLowerCase) {
+    return "Password must have at least 1 lowercase character";
+  }
+
+  if (!hasDigit) {
+    return "Password must have at least 1 digit";
+  }
+
+  if (!hasSpecialChar) {
+    return "Password must have at least 1 special character";
+  }
+
+  return true;
+}
