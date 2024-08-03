b2f16aae1329e4eef385a2abc47b27a2bf7624a2
Bruno Dias <brunodias40@gmail.com>
2024-08-03 18:43:35 -0300

    refactor: remove try-catch block in server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,10 +12,6 @@ app.use("/api", router);
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
-try {
-  app.listen(port || 3000, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
-} catch (error) {
-  console.error(error);
-}
+app.listen(port || 3000, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
