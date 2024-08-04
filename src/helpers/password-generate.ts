b5572229725b0596e846e117bf7ba0bc1c030791
Bruno Dias <brunodias40@gmail.com>
2024-08-04 16:29:28 -0300

    feat: add password generation helper function

---
+++ src/helpers/password-generate.ts
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+
+const generatePassword = (length: number = 12): string => {
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return password;
+};
+
+export const password = generatePassword();
